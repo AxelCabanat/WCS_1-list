27b876e7101c010bbe9fea98d01c7882afc4bfe2
Axel Cabanat <axelcabanat@gmail.com>
2022-05-17 10:28:26 +0200

    quest solution

---
+++ WCS_1-list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WCS_1_list
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> stringCollection = new List<string>();
+            stringCollection.Add("one");
+            stringCollection.Add("two");
+            stringCollection.Add("three");
+
+            foreach (string item in stringCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            stringCollection.RemoveAt(0);
+            stringCollection.Remove("three");
+            stringCollection[0] = "Viva l'Algérie";
+
+            foreach (string item in stringCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
